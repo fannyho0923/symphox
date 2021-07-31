0c8b86e3d6b8037d2718c4c02e70e54dd8ceec1f
howeihan <fannyho0923@gmail.com>
2021-07-31 17:41:25 +0800

    隱藏/展開的功能樹

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,31 @@
   <div>
     <nav class="nav">
       <ul class="ul">
-        <li class="li">訂單管理</li>
-        <li class="li">訂單查詢</li>
-        <li class="li">新增訂單</li>
+        <li class="togggle li" @click="togggle">訂單管理</li>
+        <li v-show="isShow" class="li">訂單查詢</li>
+        <li v-show="isShow" class="li">新增訂單</li>
       </ul>
     </nav>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isShow: false
+    };
+  },
+  methods: {
+    togggle() {
+      if (this.isShow) {
+        this.isShow = false;
+      } else {
+        this.isShow = true;
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
@@ -30,5 +45,19 @@ li + li {
 /* 項目 */
 .li {
   cursor: pointer;
+}
+.togggle::before {
+  position: absolute;
+  left: -16%;
+  top: 50%;
+  content: "";
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 5px 4px 0 4px;
+  border-color: #000000 transparent transparent transparent;
+}
+.togggle {
+  position: relative;
 }
 </style>
