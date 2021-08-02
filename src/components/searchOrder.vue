5453845e002d13bf1c943d0f60b4a4687e101804
howeihan <fannyho0923@gmail.com>
2021-08-02 14:51:38 +0800

    可據狀態status分類為進行中與已完成，或全部顯示

--- src/components/searchOrder.vue
+++ src/components/searchOrder.vue
@@ -1,7 +1,193 @@
-<template> </template>
+<template>
+  <div>
+    <div class="baseBody">
+      <nav class="nav">
+        <p>訂單狀態：</p>
+        <div class="triangle">
+          <select v-model="selected">
+            <option disabled value="0">Select</option>
+            <option value="1">進行中</option>
+            <option value="2">已完成</option>
+            <option value="3">全部顯示</option>
+          </select>
+        </div>
+      </nav>
+      <table class="bottom-line">
+        <table class="table" v-if="selected == 1 || selected == 3">
+          <tr>
+            <th><p class="th">進行中</p></th>
+          </tr>
+          <div v-if="arr1">
+            <tr class="item " v-for="(item, index) in arr1" :key="index">
+              <div class="circle">
+                <img class="logo" :src="arr1[index].logo" alt="" />
+              </div>
+              <div class="content">
+                <div class="tit">
+                  <p class="green-text">{{ arr1[index].status.type }}</p>
+                  <p>預計出貨：{{ arr1[index].date }}</p>
+                </div>
+                <div>{{ arr1[index].name }}</div>
+              </div>
+              <div>></div>
+            </tr>
+          </div>
+        </table>
+
+        <table class="table " v-if="selected == 2 || selected == 3">
+          <tr>
+            <th><p class="th">已完成</p></th>
+          </tr>
+          <div v-if="arr2">
+            <tr class="item " v-for="(item, index) in arr2" :key="index">
+              <div class="circle">
+                <img class="logo gray-color" :src="arr2[index].logo" alt="" />
+              </div>
+              <div class="content">
+                <div class="tit">
+                  <p>{{ arr2[index].status.type }}</p>
+                </div>
+                <div>{{ arr2[index].name }}</div>
+              </div>
+              <div>></div>
+            </tr>
+          </div>
+        </table>
+      </table>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  props: {
+    arr1: {
+      type: Array
+    },
+    arr2: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      selected: 0
+    };
+  },
+  updated() {},
+  created() {},
+  computed: {},
+  methods: {}
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav {
+  /* background-color: cyan; */
+  width: fit-content;
+  display: flex;
+}
+/* 下拉式選單 */
+select {
+  position: relative;
+  /*Chrome和Firefox裡面的邊框是不一樣的，所以複寫了一下*/
+  border: solid 1px #000;
+  /*很關鍵：將預設的select選擇框樣式清除*/
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  /*將背景改為綠色*/
+  /* background: green; */
+  /*留出一點位置，避免被文字覆蓋*/
+  padding: 0.5rem 1rem 0.5rem 0.5rem;
+  height: fit-content;
+}
+/*清除ie的預設選擇框樣式清除，隱藏下拉箭頭*/
+/* select::-ms-expand {
+  display: none;
+} */
+.triangle {
+  position: relative;
+  height: fit-content;
+}
+/* 小三角形 */
+.triangle::after {
+  position: absolute;
+  left: 79%;
+  top: 45%;
+  content: "";
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 5px 6px 0 6px;
+  border-color: #000000 transparent transparent transparent;
+}
+.select-hide {
+  display: none;
+}
+.table {
+  width: 350px;
+
+  /* background-color: darkorchid; */
+}
+.baseBody {
+  display: flex;
+}
+/* 狀態圓圈 */
+.circle {
+  width: 20%;
+  /* border: solid 1px; */
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+}
+.logo {
+  max-width: 100%;
+  width: 70%;
+  object-fit: contain;
+  object-position: top;
+}
+.item {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-color: #cdcdcd;
+  border-style: solid;
+  border-width: 0 1px 0 1px;
+  padding: 0.7rem 0;
+}
+.item + .item {
+  border-top: solid 1px #cdcdcd;
+}
+
+/* 狀態與時間 */
+.tit {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+}
+/* 內容 */
+.content {
+  width: 70%;
+}
+th {
+  /* border: solid 0.5px gray; */
+  background-color: #f5f5f5;
+  box-shadow: 0 0 0.7px black;
+  text-align: left;
+  padding: 0.5rem 0 0.5rem 1rem;
+}
+.th {
+  border-left: solid 3px #47b134;
+  padding-left: 0.3rem;
+}
+/* 底線工具 */
+.bottom-line {
+  border-bottom: solid 1px #cdcdcd;
+}
+.green-text {
+  color: #3e9c2d;
+}
+.gray-color {
+  filter: grayscale(100%);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,75 @@
 <template>
   <div>
-    <nav class="nav">
-      <ul class="ul">
-        <li class="togggle li" @click="togggle">訂單管理</li>
-        <li v-show="isShow" class="li">訂單查詢</li>
-        <li v-show="isShow" class="li">新增訂單</li>
-      </ul>
-    </nav>
+    <div class="container">
+      <nav class="nav">
+        <ul class="ul">
+          <li class="togggle li" @click="togggle">訂單管理</li>
+          <li v-show="isShow" class="li" @click="searchOrder">訂單查詢</li>
+          <li v-show="isShow" class="li">新增訂單</li>
+        </ul>
+      </nav>
+      <div v-if="showSearch" class="selectBox">
+        <SearchOrder :arr1="arr1" :arr2="arr2" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SearchOrder from "@/components/searchOrder";
+import AddOrder from "@/components/AddOrder";
 export default {
   data() {
     return {
-      isShow: false
+      isShow: false,
+      showSearch: false,
+      showAdd: false,
+      data: null,
+      arr1: [],
+      arr2: []
     };
+  },
+  components: {
+    SearchOrder,
+    AddOrder
+  },
+  watch: {
+    isShow() {
+      if (!this.isShow) {
+        this.showSearch = false;
+      }
+    }
   },
   created() {
     this.axios.get("../../static/data.json").then(response => {
       //   this.listData = response.data;
+      this.data = response.data;
       console.log(response.data);
+      for (var index = 0; index < this.data.length; index++) {
+        if (
+          this.data[index].status.code > 0 &&
+          this.data[index].status.code < 3
+        ) {
+          this.arr1.push(this.data[index]);
+        } else {
+          this.arr2.push(this.data[index]);
+        }
+      }
     });
   },
   methods: {
+    // 展開功能
     togggle() {
       if (this.isShow) {
         this.isShow = false;
       } else {
         this.isShow = true;
+      }
+    },
+    // 查看訂單
+    searchOrder() {
+      if (this.isShow) {
+        this.showSearch = true;
       }
     }
   }
@@ -38,23 +79,28 @@ export default {
 <style scoped>
 /* 表單外框 */
 .nav {
+  height: fit-content;
   display: inline-block;
   margin: 1.5rem 0 0 2.5rem;
   border: solid 1px;
+  padding-bottom: 1rem;
+  padding-left: 0.5rem;
 }
 .ul {
-  margin: 0.5rem 3rem 1.5rem 1rem;
+  margin: 0 0 0 1rem;
 }
 li + li {
-  margin-top: 1rem;
+  /* margin-top: 1rem; */
 }
 /* 項目 */
 .li {
+  /* background-color: cornflowerblue; */
   cursor: pointer;
+  padding: 0.5rem 3rem 0.5rem 0;
 }
 .togggle::before {
   position: absolute;
-  left: -16%;
+  left: -12%;
   top: 50%;
   content: "";
   width: 0;
@@ -65,5 +111,11 @@ li + li {
 }
 .togggle {
   position: relative;
+}
+.container {
+  display: flex;
+}
+.selectBox {
+  margin-top: 1.5rem;
 }
 </style>
